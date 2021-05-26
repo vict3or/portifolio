e299b8d83a5db09a73909040b1141f17677316e5
Vict3or <oliveira.vitoco@gmail.com>
2021-05-26 13:43:45 -0300

    building api

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,60 @@
 import styles from '../styles/home.module.css'
 import Nav from '../components/Nav'
 import Footer from '../components/footer'
+import { useEffect, useState } from 'react'
 function Home() {
+
+  const [storeData, setStoreData] = useState('')
+  const [isSearched, setIsSearched] = useState(false)
+
+
+  //   const api = {
+  //     api_key: '20210526132754',
+  //     url: 'https://pixabay.com/api/'
+  //   }
+  useEffect(() => {
+    fetch('https://portifolio-bice-three.vercel.app/api/card')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+        // setStoreData(() => data)
+      })
+      // .catch((error) => console.log(error))
+  },[])
+
 
   return (
     <div>
       <Nav />
-    <div className={styles.main}>
-      <div className={styles.homeWrapper}>
-        <div className={styles.descriptionContainer}>
-          <h1>Bem vindo!</h1>
-          <p>Sou um Front-End developer, amante dos quebra-cabeças e apaixonado em solucionar problemas.</p>
-        </div>
-        <div className={styles.projectsContainer}>
-          <h1>Projetos</h1>
-          <div className={styles.projectsCardContainer}>
-            <button className={styles.projectsCard}>
-              <div className={styles.imageContainer}>
-                <img src="uberlandia.jpg" alt="logo" />
-              </div>
-              <div className={styles.projectDescriptionContainer}>
-                <div className={styles.projectNameContainer}>
-                  <h3>Nome</h3>
-                  <div className={styles.logoContainer}>
-                    <a href="https://github.com/vict3or"><img src="github-sign.png" /></a>
-                    <a href="https://www.linkedin.com/in/victor-ribeiro-oliveira/"><img src="link-symbol.png" /></a>
-                  </div>
+      <div className={styles.main}>
+        <div className={styles.homeWrapper}>
+          <div className={styles.descriptionContainer}>
+            <h1>Bem vindo!</h1>
+            <p>Sou um Front-End developer, amante dos quebra-cabeças e apaixonado em solucionar problemas.</p>
+          </div>
+          <div className={styles.projectsContainer}>
+            <h1>Projetos</h1>
+            <div className={styles.projectsCardContainer}>
+              <button className={styles.projectsCard}>
+                <div className={styles.imageContainer}>
+                  <img src="uberlandia.jpg" alt="logo" />
                 </div>
-                <p>Description descriptiondfddfdfd iptiondescripti ondescription</p>
-              </div>
-            </button>
+                <div className={styles.projectDescriptionContainer}>
+                  <div className={styles.projectNameContainer}>
+                    <h3>Nome</h3>
+                    <div className={styles.logoContainer}>
+                      <a href="https://github.com/vict3or"><img src="github-sign.png" /></a>
+                      <a href="https://www.linkedin.com/in/victor-ribeiro-oliveira/"><img src="link-symbol.png" /></a>
+                    </div>
+                  </div>
+                  <p>Description descriptiondfddfdfd iptiondescripti ondescription</p>
+                </div>
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <Footer />
+      <Footer />
     </div>
   )
 }

a4f97d6857c50343037b3a4fb843670b725bd220
Vict3or <oliveira.vitoco@gmail.com>
2021-05-27 15:12:31 -0300

    improving logo display

--- components/Nav.js
+++ components/Nav.js
@@ -8,8 +8,7 @@ export default function Nav(props) {
     <div className={styles.navContainer}>
       <div className={styles.navbar}>
         <div className={styles.logo}>
-         <Link href="/"><img src="/victor-logo-white.png" alt="logo"/></Link> 
-          <h1>Victor Oliveira</h1>
+         <Link href="/"><img src="/victor-horizontal-white.png" alt="logo"/></Link> 
         </div>
         <div className={styles.pages}>
           <Link href="/" ><a style={{color: props.home}}>Home</a></Link>

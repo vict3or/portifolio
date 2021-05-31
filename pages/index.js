a22bf4a4cb1ecc3901a0eba15dc02b74ca5ffbd5
Vict3or <oliveira.vitoco@gmail.com>
2021-05-31 15:02:58 -0300

    get personal projects from prismic

--- pages/index.js
+++ pages/index.js
@@ -1,53 +1,48 @@
-import styles from '../styles/home.module.css'
-import Nav from '../components/Nav'
 import Footer from '../components/footer'
-import { useEffect, useState } from 'react'
-function Home() {
+import Nav from '../components/Nav'
 
-  const [storeData, setStoreData] = useState('')
-  const [isFetched, setIsFetched] = useState(false)
+import styles from '../styles/home.module.css'
 
-  useEffect(() => {
-    fetch('https://portifolio-bice-three.vercel.app/api/card')
-      .then((response) => response.json())
-      .then((data) => {
-        setStoreData(() => data)
-        setIsFetched(() => true)
-      })
-      .catch((error) => console.log(error))
-  }, [])
+import { getProjects } from '../lib/api'
 
+function Home({ projects }) {
   return (
     <div>
-      <Nav home="#eeeeee"/>
+      <Nav home="#eeeeee" />
       <div className={styles.main}>
         <div className={styles.homeWrapper}>
           <div className={styles.descriptionContainer}>
             <h1>Bem vindo!</h1>
             <p>Sou um Front-End developer, amante dos quebra-cabeças e apaixonado em solucionar problemas.</p>
           </div>
+
           <div className={styles.projectsContainer}>
             <h1>Projetos</h1>
+
             <div className={styles.projectsCardContainer}>
-              {isFetched && storeData.map(data => {
-                return (
-                  <div className={styles.projectsCard} key={data['id']}>
-                    <div className={styles.imageContainer}>
-                      <img src={data['image']} alt={data['alt']} />
-                    </div>
-                    <div className={styles.projectDescriptionContainer}>
-                      <div className={styles.projectNameContainer}>
-                        <h3>{data['name']}</h3>
-                        <div className={styles.logoContainer}>
-                          <a href={data['github']} target="_blank"><img src="github-sign.png" /></a>
-                          <a href={data['link']} target="_blank"><img src="link-symbol.png" /></a>
-                        </div>
-                      </div>
-                      <p>{data['description']}</p>
-                    </div>
+              {projects.map(project =>
+                <div className={styles.projectsCard} key={project.id}>
+                  <div className={styles.imageContainer}>
+                    <img src={project.image?.url} alt={project.image?.alt} />
                   </div>
-                )
-              })}
+
+                  <div className={styles.projectDescriptionContainer}>
+                    <div className={styles.projectNameContainer}>
+                      <h3>{project.name}</h3>
+
+                      <div className={styles.logoContainer}>
+                        <a href={project.repository} target="_blank">
+                          <img src="github-sign.png" />
+                        </a>
+                        <a href={project.url} target="_blank">
+                          <img src="link-symbol.png" />
+                        </a>
+                      </div>
+                    </div>
+                    <p>{project.description}</p>
+                  </div>
+                </div>
+              )}
             </div>
           </div>
         </div>
@@ -55,6 +50,17 @@ function Home() {
       <Footer />
     </div >
   )
+}
+
+export async function getStaticProps() {
+  const projects = await getProjects()
+
+  return {
+    props: {
+      projects,
+    },
+    revalidate: 60
+  }
 }
 
 export default Home
